43b67ac65d5f4f5104d5229fc836baa38c33a696
Rich Wareham <rjw57@cam.ac.uk>
2019-01-28 09:26:33 +0000

    settings.docker: add more verbose logging
    Copy the logging configuration from other projects to ensure that
    exceptions are recorded in the logs.

--- project/settings/docker.py
+++ project/settings/docker.py
@@ -8,3 +8,32 @@ DEBUG = False
 
 # All hosts are allowed to connect to the container by default
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(levelname)s %(asctime)s %(pathname)s:%(lineno)d '
+                       '%(funcName)s "%(message)s"')
+        },
+        'simple': {
+            'format': '%(levelname)s "%(message)s"'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO'
+        },
+    }
+}
